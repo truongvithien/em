39d3d558924be0bf0cfa62bb38c2863b80e0a255
Trương Vĩ Thiên <thientv.cs@gmail.com>
2023-10-18 14:56:36 +0700

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+
+const data = [
+    {
+        name: 'John Doe',
+        age: 32,
+        location: 'New York'
+    },
+    {
+        name: 'Jane Doe',
+        age: 28,
+        location: 'Chicago'
+    },
+    {
+        name: 'John Smith',
+        age: 45,
+        location: 'Los Angeles'
+    }
+];
+
+console.table(data) 
+
+console.log($("body").length);
